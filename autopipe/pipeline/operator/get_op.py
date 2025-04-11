a45255b4a83e646b0ca4dd45ddb8e97a5f4b0277
chenhaojiong <chenhaojiong@pjlab.org.cn>
2025-04-11 16:38:11 +0800

    debug of ray step

--- autopipe/pipeline/operator/get_op.py
+++ autopipe/pipeline/operator/get_op.py
@@ -7,7 +7,9 @@ def get_operator(name: str, params_dict: Optional[Dict[str, Any]] = None) -> Any
     """通过 operator_name 获取已注册的类并实例化"""
 
     if name not in _REGISTRY:
-        raise KeyError(f"Operator '{name}' not found. Available: {list(_REGISTRY.keys())}")
+        raise KeyError(
+            f"Operator '{name}' not found. Available able: {list(_REGISTRY.keys())}"
+        )
 
     operator_cls = _REGISTRY[name]
     return operator_cls(params_dict=params_dict)
